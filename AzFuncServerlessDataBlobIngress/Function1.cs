bea172a86be2f261ca7c156cf2efef9641d4c58d
Max Hutchinson <maximus258@hotmail.co.uk>
2022-01-11 16:40:24 +0000

    Content type validation

--- AzFuncServerlessDataBlobIngress/Function1.cs
+++ AzFuncServerlessDataBlobIngress/Function1.cs
@@ -7,23 +7,37 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace AzFuncServerlessDataBlobIngress
 {
-    public static class Function1
+    public static class DataBlobingress
     {
-        [FunctionName("Function1")]
+        [FunctionName("DataBlobingress")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
+            HttpRequest request,
+            Binder binder,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // Content type validation 
+
+            if (request.ContentType != "application/json")
+            {
+                return new BadRequestResult();
+            }
+
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
