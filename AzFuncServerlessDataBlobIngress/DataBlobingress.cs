5d267081c48401ddede275c867afa6c60aa64c1d
Max Hutchinson <maximus258@hotmail.co.uk>
2022-01-11 17:10:03 +0000

    cleaned up repo

--- AzFuncServerlessDataBlobIngress/DataBlobingress.cs
+++ AzFuncServerlessDataBlobIngress/DataBlobingress.cs
@@ -6,15 +6,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using System.Threading;
 
 namespace AzFuncServerlessDataBlobIngress
